38160ebc4b16b9c5fc3526968191501e2ba06653
Ari <arikhursheed@gmail.com>
2025-08-16 19:26:17 -0500

    Added gradientbackground to the assets and applied it to all views and sheets

---
+++ ScoreBoard/Utils/GradientBackgroundView.swift
@@ -0,0 +1,45 @@
+//
+//  GradientBackgroundView.swift
+//  ScoreBoard
+//
+//  Created by Ari Dawoodi on 8/15/25.
+//
+
+import SwiftUI
+
+struct GradientBackgroundView: View {
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(colors: [
+                Color("GradientBackground"), // Dark green from asset
+                Color.black // Very dark gray / almost black (#0b0b0b)
+            ]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+        .ignoresSafeArea()
+    }
+}
+
+// Extension to make it easy to apply the gradient background to any view
+extension View {
+    func gradientBackground() -> some View {
+        ZStack {
+            GradientBackgroundView()
+            self
+        }
+    }
+}
+
+#Preview {
+    VStack(spacing: 20) {
+        Text("Sample Content")
+            .foregroundColor(.white)
+            .font(.title)
+        
+        Text("This view has the gradient background")
+            .foregroundColor(.white.opacity(0.8))
+            .font(.body)
+    }
+    .gradientBackground()
+}
