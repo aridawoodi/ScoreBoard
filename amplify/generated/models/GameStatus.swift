19b9b7a9a8479012ed96a5ff23abeda324b9665b
Ari <arikhursheed@gmail.com>
2025-08-23 23:42:18 -0500

    Remove sensitive Amplify configuration files and enhance security

--- amplify/generated/models/GameStatus.swift
+++
@@ -1,9 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public enum GameStatus: String, EnumPersistable {
-  case active = "ACTIVE"
-  case completed = "COMPLETED"
-  case cancelled = "CANCELLED"
-}
--- amplify/generated/models/WinCondition.swift
+++
@@ -1,8 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public enum WinCondition: String, EnumPersistable {
-  case highestScore = "HIGHEST_SCORE"
-  case lowestScore = "LOWEST_SCORE"
-}
